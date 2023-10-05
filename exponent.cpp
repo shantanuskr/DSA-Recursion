b8cd475082263c21e1d7074eb904ded1a6ad0686
Shantanu Sarkar <86945235+shantanuskr@users.noreply.github.com>
2023-10-05 20:59:06 +0530

    Add files via upload

---
+++ exponent.cpp
@@ -0,0 +1,31 @@
+#include<iostream>
+#include<math.h>
+using namespace std;
+
+int power( int a , int b){
+    if ( b == 0)
+        return 1;
+    
+    if ( b == 1) 
+        return a;
+    
+    int ans = pow( a, b/2);
+
+
+    if ( b&2 ){
+        return ans*ans;
+    }
+    else {
+        return a*ans* ans;
+    }
+
+}
+int main (){
+
+    int a, b;
+    cin>> a >> b;
+
+    int ans = power(a, b);
+
+    cout << ans << endl;
+}
